5c846f49d0b1a7614e3640d2c5f21e79a2fb8300
Tej Birring <tejbirring@gmail.com>
2023-11-19 10:42:50 +0000

    Add Firebase Prv Key (for auth) and Client Keys to config for env load.

--- configs/index.ts
+++ configs/index.ts
@@ -3,5 +3,12 @@ import { loadRequiredEnvironmentVariable } from "./env";
 export default {
     service: {
         port: loadRequiredEnvironmentVariable("PORT", "int"),
-    }
+    },
+    firebase: {
+        privateKey: JSON.parse(loadRequiredEnvironmentVariable("FIREBASE_ADMIN_PRV_KEY_JSON", "string"))
+    },
+    clientKeys: [
+        loadRequiredEnvironmentVariable("CLIENT_KEY_FIREBASE_FUNCTIONS", "string"),
+        loadRequiredEnvironmentVariable("CLIENT_KEY_WEB_APP", "string"),
+    ]
 } as const;
